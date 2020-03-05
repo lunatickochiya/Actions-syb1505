680e1275ad281781b490edf4aba321c474cdf9dd
lunaticurey <125438787@qq.com>
2020-03-05 18:34:05 +0800

    fix packages

--- after_build.sh
+++ after_build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #=================================================
 # this script is from https://github.com/HiGarfield  
-# Writed By lunatickochiya
+# Writen By lunatickochiya
 # QQ group :286754582  https://jq.qq.com/?_wv=1027&k=5QgVYsC   
 #=================================================
 
 #cd openwrt
 mkdir out
+mkdir bin/packages
 rm -rf build_dir/target* build_dir/toolchain* staging_dir/target* staging_dir/toolchain*
 cp -u -f bin/*/*-*-squashfs-sysupgrade.bin out/
+cp -u -f bin/*/packages/* bin/packages/
 # Enter your commands here, e.g.
 echo "Copy Success!"
